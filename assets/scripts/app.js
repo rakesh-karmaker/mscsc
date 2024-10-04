1f4fdeb1b947f52d9ba77436d38a8b4149708be4
Rakesh-6610 <rakeshgaming12345@gmail.com>
2024-10-04 15:26:55 +0600

    add and style the events section

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,3 +7,26 @@ $(document).ready(() => {
 $(".navbar-toggler").click(() => {
     $(".nav-links").slideToggle("fast");
 })
+
+$(".happened").click(() => {
+    $(".all").removeClass("nav-active");
+    $(".upcoming").removeClass("nav-active");
+    $(".happened").addClass("nav-active");
+    $(".swiper-slide[status='happened']").slideDown("fast");
+    $(".swiper-slide:not([status='happened'])").slideUp("fast");
+})
+
+$(".upcoming").click(() => {
+    $(".all").removeClass("nav-active");
+    $(".happened").removeClass("nav-active");
+    $(".upcoming").addClass("nav-active");
+    $(".swiper-slide[status='upcoming']").slideDown("fast");
+    $(".swiper-slide:not([status='upcoming'])").slideUp("fast");
+})
+
+$(".all").click(() => {
+    $(".all").addClass("nav-active");
+    $(".happened").removeClass("nav-active");
+    $(".upcoming").removeClass("nav-active");
+    $(".swiper-slide").slideDown("fast");
+})
