0722c961de72746334a3bdf9183856a12bc8bc30
Rakesh-6610 <rakeshgaming12345@gmail.com>
2024-10-05 22:04:22 +0600

    add the filter function

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,36 @@ $(document).ready(() => {
 })
 
 
+const swiper = new Swiper('.swiper', {
+    centeredSlides: "auto",
+    loop: true,
+    initialSlide: 0,
+    slidesPerView: "auto",
+    grabCursor: true,
+    speed: 400,
+    spaceBetween: 40,
+    autoplay: {
+        delay: 2000,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    breakpoints: {
+        600: {
+            loop: true,
+            centeredSlides: false,
+        }
+    }
+});
+
+
+
+
+
+
+
 const removeActiveClass = (ele) => {
     for (let i = 0; i < $(".button-link").length; i++) {
         const selectedEle = $(`.button-link:nth-child(${i + 1})`);
@@ -35,25 +65,27 @@ $(".navbar-toggler").click(() => {
     $(".nav-links").slideToggle("fast");
 })
 
-$(".happened").click(() => {
-    $(".all").removeClass("nav-active");
-    $(".upcoming").removeClass("nav-active");
-    $(".happened").addClass("nav-active");
-    $(".swiper-slide[status='happened']").slideDown("fast");
-    $(".swiper-slide:not([status='happened'])").slideUp("fast");
-})
+const filter = (status) => {
+    const states = ["happened", "upcoming", "all"];
+    for (let i = 0; i < states.length; i++) {
+        const selectedEle = $(`.${states[i]}`);
+        (selectedEle === status) ? selectedEle.addClass("nav-active") : selectedEle.removeClass("nav-active");
+    }
+    if (status === "all") { 
+        $(".swiper-slide").css("display", "flex")
+        $(".swiper-slide").slideDown("fast");
+    }
+    else {
+        $(`.swiper-slide[status='${status}']`).css("display", "flex")
+        $(`.swiper-slide[status='${status}']`).slideDown("fast");
+        $(`.swiper-slide:not([status='${status}'])`).slideUp("fast");
+        $(`.swiper-slide:not([status='${status}'])`).css("display", "none");
+    }
+    swiper.updateSlides();
+    swiper.slideTo(0);
+    swiper.autoplay.start();
+}
 
-$(".upcoming").click(() => {
-    $(".all").removeClass("nav-active");
-    $(".happened").removeClass("nav-active");
-    $(".upcoming").addClass("nav-active");
-    $(".swiper-slide[status='upcoming']").slideDown("fast");
-    $(".swiper-slide:not([status='upcoming'])").slideUp("fast");
-})
-
-$(".all").click(() => {
-    $(".all").addClass("nav-active");
-    $(".happened").removeClass("nav-active");
-    $(".upcoming").removeClass("nav-active");
-    $(".swiper-slide").slideDown("fast");
-})
+$(".happened").click(() => { filter("happened") });
+$(".upcoming").click(() => { filter("upcoming") });
+$(".all").click(() => { filter("all") });
