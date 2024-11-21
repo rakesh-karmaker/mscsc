7ba18d1d35071dd53e6778c036887b6ec4d272d0
Rakesh Karmaker <rakeshgaming12345@gmail.com>
2024-11-21 10:54:38 +0600

    change the location of all the data into a seperate file and import them

---
+++ assets/scripts/data.js
@@ -0,0 +1,429 @@
+//* Below the codes are just to store the data for the executive-panel, not for to convert to React
+export const executivesData = {
+  "2024-2025": [
+    {
+      name: "Alvi Adib",
+      position: "President",
+      image: "2024-2025/alvipfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/Higashi.Yagami",
+        linkedin: "https://www.linkedin.com/in/alvi-abid-2076012a6/",
+      },
+    },
+    {
+      name: "Taharat Rahman",
+      position: "VP of Administration",
+      image: "2023-2024/taharatpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/taharat.rahman.18",
+      },
+    },
+    {
+      name: "Muhammad Saad",
+      position: "VP of Learning & Head of Physics and Math Dpt.",
+      image: "2023-2024/saadpfp.jpg",
+    },
+    {
+      name: "Raqin Shahrier",
+      position: "VP of management",
+      image: "2023-2024/raqinpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/raqin.shahrier",
+      },
+    },
+    {
+      name: "Md Tanvir Ahmed",
+      position: "General Secretary",
+      image: "2024-2025/tanvirpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/virtangog",
+        github: "https://github.com/TanvirPlayzXX",
+      },
+    },
+    {
+      name: "Arham Akhyar",
+      position: "Assistant General Secretary",
+      image: "2024-2025/arhampfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/arham.akhyar.31",
+      },
+    },
+    {
+      name: "Alif Khandaker",
+      position: "Organizing Secretary",
+      image: "2024-2025/alifpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/profile.php?id=100026608211528",
+      },
+    },
+    {
+      name: "Mohammed Safoan",
+      position: "Assistant Organizing Secretary",
+      image: "2024-2025/safoanpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/profile.php?id=100091412962644",
+      },
+    },
+    {
+      name: "Rakesh Karmaker",
+      position: "Web Developer",
+      image: "2024-2025/rakeshpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/rakesh.karmaker.980",
+        github: "https://github.com/rakesh-karmaker",
+      },
+    },
+    {
+      name: "Shaikh Sadi",
+      position: "Graphics Designer",
+      image: "2024-2025/sadipfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/ShaikhSadi2008",
+      },
+    },
+    {
+      name: "Zannatul Zafrin",
+      position: "Press and Publication",
+      image: "2024-2025/zannatulpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/profile.php?id=100058734297302",
+      },
+    },
+  ],
+
+  "2023-2024": [
+    {
+      name: "Nur Saba",
+      position: "President",
+      image: "placeholderpfp.jpg",
+      socials: {},
+    },
+    {
+      name: "Faiza Karim",
+      position: "General Secretary",
+      image: "placeholderpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/faizakarim1311",
+      },
+    },
+    {
+      name: "Turjo Ghosh",
+      position: "Organizing Secretary",
+      image: "2023-2024/turjopfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/profile.php?id=100074396814474",
+      },
+    },
+    {
+      name: "Raqin Shahrier",
+      position: "Assistant General Secretary",
+      image: "2023-2024/raqinpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/raqin.shahrier",
+      },
+    },
+    {
+      name: "Muhammad Saad",
+      position: "Assistant General Secretary",
+      image: "2023-2024/saadpfp.jpg",
+      socials: {},
+    },
+    {
+      name: "Ahanaf Muntasir",
+      position: "Assistant General Secretary",
+      image: "placeholderpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/MD.AHANAF.4741",
+      },
+    },
+    {
+      name: "MD. Sheam Haque",
+      position: "Assistant Organizing Secretary",
+      image: "placeholderpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/mdsheam.haque",
+      },
+    },
+    {
+      name: "Taharat Rahman",
+      position: "Assistant Organizing Secretary",
+      image: "2023-2024/taharatpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/taharat.rahman.18",
+      },
+    },
+    {
+      name: "Adnan Adib",
+      position: "Assistant Organizing Secretary",
+      image: "2023-2024/adnanpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/kamaboko.gonpachiro.393",
+      },
+    },
+    {
+      name: "Dhrubo Nur Ahammed",
+      position: "Head of IT & Graphics Team",
+      image: "placeholderpfp.jpg",
+      socials: {
+        facebook: "https://www.facebook.com/the.legendary.boy.134",
+      },
+    },
+    {
+      name: "Kazi Maisha",
+      position: "Secretary of Press and Publication",
+      image: "placeholderpfp.jpg",
+      socials: {},
+    },
+    {
+      name: "Sadaf Bin Azhar",
+      position: "Secretary of Press and Publication",
+      image: "placeholderpfp.jpg",
+      socials: {},
+    },
+  ],
+};
+
+//* Below the codes are just to store the data for the activities, not for to convert to React
+export const activitiesData = [
+  {
+    tag: "workshop",
+    activityDate: "11-10-2024",
+    activityImagePath: "psycho&humanbrain.jpg",
+    activityTitle: "Psychology & Human Brain",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02v6CpPbgH94nYqVVTqEQWAyP5eDKbT8YT4JHREuovxBw8zXvhfQCpMMEv4gdC2hrJl",
+  },
+  {
+    tag: "workshop",
+    activityDate: "23-08-2024",
+    activityImagePath: "psandsd.jpg",
+    activityTitle: "Problem Solving and Skill Development",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid033NC7YhySMq2xF8wZQNTDSzaB9SMzkn7dLebpUbMcwSDRWWvfJcYTWKppLSLDBdcwl",
+  },
+  {
+    tag: "workshop",
+    activityDate: "16-08-2024",
+    activityImagePath: "newtonian.jpg",
+    activityTitle: "Newtonian Mechanics",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02ExP6BMGRua3Uw9qpxAjqnBQCuwA2RDxaJJk2qdbggiDdTnh3YCEU1RLiTTsWArGSl",
+  },
+  {
+    tag: "article",
+    activityDate: "21-05-2024",
+    activityImagePath: "analyticalchemistry.jpg",
+    activityTitle: "Analytical Chemistry",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02bvKttcr2XvPNQYYEvP3mM9ULvQoL3YntV1ERV3uVGzgnLMGpFGPAA2TQut1wqohTl",
+  },
+  {
+    tag: "article",
+    activityDate: "23-04-2024",
+    activityImagePath: "europa.jpg",
+    activityTitle: "ARE WE ALONE IN THE UNIVERSE?",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02asPSimLtLt7pHSYa62sQMATDdCbQZHN1mxeckCFEtYaN8hyaYEnpv9DbirVKoZuyl",
+  },
+  {
+    tag: "article",
+    activityDate: "21-04-2024",
+    activityImagePath: "neutronstars.jpg",
+    activityTitle: "Neutron Stars",
+    activityDesc: `Neutron stars are the remains of the cores of massive stars that have reached the end of their lives. There are 2 possible evolutionary endpoints of the massive stars, they can be either black holes or neutron stars. Astronomers consider the mass of a neutron star to range from 1.4 to 2.9 solar masses.
+A neutron star is the densest object astronomers can observe directly. It was discovered by Jocelyn Bell Burnell in 1967.
+There are several types of neutron stars, including magnetars and pulsars. There are thought to be around one billion neutron stars in the Milky Way. Only 31 magnetars have ever been discovered and over 3000 pulsars have been discovered.`,
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid027RYKXSgpbCajg7kCfcKjZPm1CACL2LwpSuCiz1npGctZvfhBchHUn36jtVEDfmwVl",
+  },
+  {
+    tag: "achievement",
+    activityDate: "22-03-2024",
+    activityImagePath: "1strunnersupbdmo.jpg",
+    activityTitle: "1st Runner Up at BDMO",
+    activityDesc:
+      "MSCSC is representing itself in its fullest on the national field, Muhammad Saad from MSCSC has become 1st runners-up in the national round secondary category of BDMO. Through these successes, MSCSC is effectively displaying the talent and skills of our members. We can only hope for even more prosperous days in the future of MSCSC.",
+    activityLink:
+      "https://www.facebook.com/photo.php?fbid=811658817668493&set=a.461248879376157&type=3",
+  },
+  {
+    tag: "achievement",
+    activitiesDate: "22-03-2024",
+    activityImagePath: "2ndrunnersupbdbo.jpg",
+    activityTitle: "2nd Runner Up at BDBO",
+    activityDesc:
+      "MSCSC is representing itself in its fullest on the national field, Raqin Shahrier has become the 2nd regional runners-up of BDBO. Through these successes, MSCSC is effectively displaying the talent and skills of our members. We can only hope for even more prosperous days in the future of MSCSC.",
+    activityLink:
+      "https://www.facebook.com/photo/?fbid=811658864335155&set=pcb.811658881001820",
+  },
+  {
+    tag: "achievement",
+    activitiesDate: "22-03-2024",
+    activityImagePath: "21statbdoaa.jpg",
+    activityTitle: "21st position at BDOAA",
+    activityDesc:
+      "MSCSC is steadily gaining recognition in the biggest of stages. Alvi Abid from MSCSC has gotten the 21st position in BDOAA and will be attending the national camp. His skills and sheer talent have brought him to such heights and represented MSCSC in such high ranks. We sincerely hope that the members of MSCSC will keep bringing these successes and represent MSCSC in the highest positions.",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid0B9865y5CTuQWm1hWZWYn13mLxQiYW5zqKK84w6jMTmszyLbNVsNhThVjbgiqdJXcl",
+  },
+  {
+    tag: "workshop",
+    activityDate: "02-03-2024",
+    activityImagePath: "wave-nature.jpg",
+    activityTitle: "The Wave Nature of Light",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02myHnJTczAxY8i1rzNfh4MSLUf5gAeMmk29iHYXFy362M23HyHvN3zFN4UtTndN1tl",
+  },
+  {
+    tag: "workshop",
+    activityDate: "16-12-2023",
+    activityImagePath: "logicbehindcomputers.jpg",
+    activityTitle: "Introduction to the logic behind",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid033rFyVZJR7Z1dQxp1UXhkTe55JkBSFfE7VzjWaeSP8C1UJziZk4ZY3NuAeBNPvE3ql",
+  },
+  {
+    tag: "workshop",
+    activityDate: "05-05-2023",
+    activityImagePath: "hawkingraditionbigbangcipa.jpg",
+    activityTitle: "Hawking Radiation, Big Bang, CIPA",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid06ymY657GmMvKjSXH8wsKkaP34nACtLMtUXhyerTpxwEhUDwL8nswtnBK5cEFSWgul",
+  },
+  {
+    tag: "achievement",
+    activityDate: "07-03-2023",
+    activityImagePath: "3rdndcmathcarnivalchampion.jpg",
+    activityTitle: "3rd NDC Math Carnival Wall Magazine Champion",
+    activityDesc:
+      "It's a great pleasure to announce that team Infinity contained with Turjo Ghosh, Mohammad Safoan & Saran Chowdhury became champions of Math Relay & Wall Magazine in the 3rd Annual Notre Dame Math Festival 2023. MSCSC is only growing immensely since it's an expedition of science that started back in 2017. Regardless of the magnitude of the milestones, MSCSC believes that every achievement is worth celebrating.",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02DGRKoZGbAEYWyxHLhQS3db6qgD7sSsWcKwE2xrdg7kcgqRUF3Wyb5niirysK59FVl",
+  },
+  {
+    tag: "workshop",
+    activityDate: "24-01-2023",
+    activityImagePath: "matholympiad.jpg",
+    activityTitle: "Math Olympiad",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02JxEZscZxqRQA6xmh1GRhFkt1GM9phXT1b9xwxx95h8DeEspsoHcP9y28myZPPEoel",
+  },
+  {
+    tag: "workshop",
+    activityDate: "20-01-2023",
+    activityImagePath: "projectbuilding.jpg",
+    activityTitle: "Project Building",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid02oQcnpL88MkaUJhpKAoUGPjaqBoMyHWz1bTHnbeszSE6EbXSVxQRdbM2rSQAeaMr3l",
+  },
+  {
+    tag: "event",
+    activityDate: "26-01-2023",
+    activityImagePath: "intraschool-2023.png",
+    activityTitle: "Intraschool exploration (2023)",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/photo/?fbid=544333784400999&set=a.461248859376159",
+  },
+  {
+    tag: "event",
+    activityDate: "23-09-2022",
+    activityImagePath: "scitonic2.0.jpg",
+    activityTitle: "Scitonic 2.0",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/photo.php?fbid=461248866042825&set=pb.100064731457622.-2207520000&type=3",
+  },
+  {
+    tag: "achievement",
+    activityDate: "29-06-2022",
+    activityImagePath: "1st3rdphysicsoly.png",
+    activityTitle: "1st place in Extempore Speech and 3rd in Physics Olympiad",
+    activityDesc:
+      "It is with pleasure that we announce that two beloved members of our MSCSC family, Md. Tarekuzzaman Tarek and Aziza Shadaf Etha have achieved recognition in ড্রিঙ্কো '২য় জাতীয় বিজ্ঞান উৎসব ২০২২' পাওয়ার্ড বাই ওয়ালটন ল্যাপটপ। Tarek secured 1st place in Extempore Speech and Etha stood 3rd in Physics Olympiad at the event. MSCSC congratulates them on their success and wishes them all the best for their future endeavours💚",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid0EaeeUEySbMz2L4Vs4sdxpk7toLq467LxEK6mXLgDoESGxWAAZjZgPbV3gpnX8kojl",
+  },
+  {
+    tag: "event",
+    activityDate: "19-08-2021",
+    activityImagePath: "chemistrycampaign.jpg",
+    activityTitle: "Chemistry Campaign",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/photos/a.199847797167738/1169562713529570/?type=3",
+  },
+  {
+    tag: "workshop",
+    activityDate: "29-07-2021",
+    activityImagePath: "robotics.png",
+    activityTitle: "Robotics",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid025DmM5ZHcEiUWDFTEnkBnepp5omiZ8h7uW3HtgjoXr15t7uwtZuCKhBEiCfLZPvP3l",
+  },
+  {
+    tag: "event",
+    activityDate: "07-07-2021",
+    activityImagePath: "astrocampaign.jpg",
+    activityTitle: "Astro Campaign",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/photos/a.199847797167738/1138074580011717/?type=3",
+  },
+  {
+    tag: "achievement",
+    activityDate: "28-05-2021",
+    activityImagePath: "drmc2nd.png",
+    activityTitle: "2nd at DRMC Virtual National Science Carnival-2021",
+    activityDesc:
+      'Albeit a little belated, we are proudly announcing that one of our most diligent members of MSCSC, Mushfiq Zubayer, has placed second in the Digital Poster Designing segment of DRMC Science Club Presents "DRMC Virtual National  Science Carnival-2021".',
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/posts/pfbid0dpQqfNA7byZQb9YkKt4iV6K3aHcEnN2Y9F5MLvwMGCF8RbvsLMMhgBS9m2k6X21cl",
+  },
+  {
+    tag: "event",
+    activitiesDate: "03-03-2021",
+    activityImagePath: "mathtcsquest.png",
+    activityTitle: "Mathycs Quest",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/MSCSC2014/photos/pb.100064731457622.-2207520000/1057644704721372/?type=3",
+  },
+  {
+    tag: "event",
+    activityDate: "02-10-2020",
+    activityImagePath: "science&cosmicspace.png",
+    activityTitle: "Science & Cosmic Space",
+    activityDesc:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus aliquid officiis provident ad molestiae consequatur facere odit voluptas veniam dignissimos!",
+    activityLink:
+      "https://www.facebook.com/photo?fbid=951179522034558&set=pb.100064731457622.-2207520000",
+  },
+];
