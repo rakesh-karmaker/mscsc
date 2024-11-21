4bf268fa749645f0ea76da7144f94df3ed2804ee
Rakesh Karmaker <rakeshgaming12345@gmail.com>
2024-11-21 09:55:19 +0600

    add a working pagination in the activities page

--- assets/scripts/activities.js
+++ assets/scripts/activities.js
@@ -252,76 +252,138 @@ const getActivities = (tag) => {
   return activitiesData.filter((activity) => activity.tag === tag);
 };
 
-const generateActivityHtml = (activity, selected) => {
-  let activityTagHTML = "";
-  if (!selected) {
-    activityTagHTML = `
-        <p class="activity-tag">
-            <i class="fa-solid fa-chalkboard-user"></i>
-            <span>${activity.tag}</span>
-        </p>
-      `;
+const generateActivityHTML = (
+  {
+    tag,
+    activityDate,
+    activityImagePath,
+    activityTitle,
+    activityDesc,
+    activityLink,
+  },
+  showTag
+) => {
+  let tagHTML = "";
+  if (showTag) {
+    tagHTML = `
+      <p class="activity-tag">
+        <i class="fa-solid fa-chalkboard-user"></i>
+        <span>${tag}</span>
+      </p>
+    `;
   }
   return `
-    <div class="activity" tag="${activity.tag}">
-        <img
-        src="../assets/images/activities/${activity.tag}/${activity.activityImagePath}"
-        alt="The image of ${activity.activityTitle}"
-        />
+    <div class="activity" tag="${tag}">
+      <img
+        src="../assets/images/activities/${tag}/${activityImagePath}"
+        alt="The image of ${activityTitle}"
+      />
 
-        <article>
+      <article>
         <div>
-            ${activityTagHTML}
-            <p class="activity-date">
-                <i class="fa-regular fa-calendar"></i> <span>11-10-2024</span>
-            </p>
+          ${tagHTML}
+          <p class="activity-date">
+            <i class="fa-regular fa-calendar"></i> <span>${activityDate}</span>
+          </p>
         </div>
-        <h2 class="activity-name">${activity.activityTitle}</h2>
-        <p class="secondary-text activity-summary">${activity.activityDesc}</p>
-        <a href="${activity.activityLink}" class="primary-button"
-            >Learn More <i class="fa-solid fa-arrow-right"></i
+        <h2 class="activity-name">${activityTitle}</h2>
+        <p class="secondary-text activity-summary">${activityDesc}</p>
+        <a href="${activityLink}" class="primary-button"
+          >Learn More <i class="fa-solid fa-arrow-right"></i
         ></a>
-        </article>
+      </article>
     </div>
-    `;
+  `;
 };
 
-const displayActivities = (tag, selectedActiticities) => {
+const renderActivities = (activities, showTag) => {
   const activitiesContainer = document.querySelector(".activities-container");
   activitiesContainer.innerHTML = "";
-  selectedActiticities.forEach((activity) => {
-    activitiesContainer.innerHTML += generateActivityHtml(
-      activity,
-      tag !== "all"
-    );
+  activities.forEach((activity) => {
+    activitiesContainer.innerHTML += generateActivityHTML(activity, showTag);
   });
 };
 
-const getURLTag = () => {
-  const url = window.location.href;
-  return !url.includes("?tag=") ? "all" : url.split("?tag=")[1];
+const getUrlTag = () => {
+  const url = new URL(window.location.href);
+  const tag = url.searchParams.get("tag");
+  return tag ?? "all";
 };
 
-let paginationStartingPoint = 0;
-let paginationEndingPoint = 12;
-const activitiesMain = () => {
-  const tag = getURLTag();
+const activitiesMain = (selectedTag = getUrlTag()) => {
   document.querySelectorAll(".activities-nav-link").forEach((link) => {
-    link.getAttribute("nav-type") === tag
-      ? link.classList.add("active")
-      : link.classList.remove("active");
+    const navType = link.getAttribute("nav-type");
+    link.classList.toggle("active", navType === selectedTag);
   });
 
-  const allTagedAtivities = getActivities(tag);
-  let selectedActiticities = allTagedAtivities;
-  if (allTagedAtivities.length >= 12) {
-    selectedActiticities = allTagedAtivities.slice(
-      paginationStartingPoint,
-      paginationEndingPoint
-    );
+  const activities = getActivities(selectedTag);
+  const isPaginationAvailable = activities.length >= 12;
+  $(".pagination").attr("available", isPaginationAvailable.toString());
+
+  if (isPaginationAvailable) {
+    initPagination(activities);
+  } else {
+    renderActivities(activities, selectedTag);
+  }
+};
+
+const initPagination = (activities) => {
+  const pagination = document.querySelector(".pagination");
+  pagination.setAttribute("available", "true");
+
+  const pageNavContainer = document.querySelector(".page-nav-container");
+  pageNavContainer.innerHTML = "";
+  const totalPages = Math.ceil(activities.length / 12);
+  for (let i = 1; i <= totalPages; i++) {
+    const pageNumberElement = document.createElement("button");
+    pageNumberElement.classList.add("page-number", "row-center");
+    if (i === 1) {
+      pageNumberElement.classList.add("page-active");
+    }
+    pageNumberElement.setAttribute("nav-type", i);
+    pageNumberElement.setAttribute("page-number", i);
+    pageNumberElement.textContent = i;
+    pageNumberElement.addEventListener("click", () => changePage(i));
+    pageNavContainer.appendChild(pageNumberElement);
   }
 
-  displayActivities(tag, selectedActiticities);
+  changePage(1);
+};
+
+const changePage = (pageNumber) => {
+  const startIndex = 12 * (pageNumber - 1);
+  const endIndex = 12 * pageNumber;
+  const allTaggedActivities = getActivities(getUrlTag());
+  const selectedActivities = allTaggedActivities.slice(startIndex, endIndex);
+  renderActivities(selectedActivities, getUrlTag());
+  updatePagination(pageNumber, allTaggedActivities.length);
+};
+
+const updatePagination = (pageNumber, activitiesNumber) => {
+  const pageNumbers = document.querySelectorAll(".page-number");
+  const minimumPageNumber = 1;
+  const maximumPageNumber = Math.ceil(activitiesNumber / 12);
+
+  pageNumbers.forEach((page) =>
+    page.classList.toggle(
+      "page-active",
+      page.getAttribute("page-number") == pageNumber
+    )
+  );
+
+  const nextButton = document.querySelector(".pagination-btn[action='next']");
+  const prevButton = document.querySelector(".pagination-btn[action='prev']");
+
+  if (pageNumber === minimumPageNumber) {
+    nextButton.classList.remove("hidden");
+    prevButton.classList.add("hidden");
+  } else if (pageNumber === maximumPageNumber) {
+    prevButton.classList.remove("hidden");
+    nextButton.classList.add("hidden");
+  } else {
+    prevButton.classList.remove("hidden");
+    nextButton.classList.remove("hidden");
+  }
 };
 
 $().ready(() => {
