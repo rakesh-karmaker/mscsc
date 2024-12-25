6a00fda9c366bc5ec042d7dbf8b6c67bc0020e99
Rakesh Karmaker <rakeshgaming12345@gmail.com>
2024-12-25 11:35:07 +0600

    add text code for examine render

--- server/middleware/multer.js
+++ server/middleware/multer.js
@@ -2,14 +2,18 @@ const multer = require("multer");
 const path = require("path");
 
 exports.uploadSingle = (folderName) => {
-  return (imageUpload = multer({
-    storage: multer.diskStorage({
-      destination: function (req, file, cb) {
-        cb(null, `public/${folderName}`);
-      },
-      filename: function (req, file, cb) {
-        cb(null, `${Date.now()}-${file.originalname}`);
-      },
-    }),
-  }));
+  try {
+    return (imageUpload = multer({
+      storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, `public/${folderName}`);
+        },
+        filename: function (req, file, cb) {
+          cb(null, `${Date.now()}-${file.originalname}`);
+        },
+      }),
+    }));
+  } catch (error) {
+    console.error("Error uploading image:", error);
+  }
 };
