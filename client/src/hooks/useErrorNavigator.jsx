4fc899afb3e14b278f39a6ad6be609a324d3d71c
Rakesh Karmaker <rakeshgaming12345@gmail.com>
2025-01-15 20:49:09 +0600

    remove the highlight effect of button and other clickable elements

--- client/src/hooks/useErrorNavigator.jsx
+++ client/src/hooks/useErrorNavigator.jsx
@@ -4,23 +4,24 @@ import { useNavigate } from "react-router-dom";
 const useErrorNavigator = (isError, error) => {
   const navigate = useNavigate();
   useEffect(() => {
-    if (isError) {
-      if (error?.status === 400) {
-        navigate("/400", { replace: true });
-      }
-      if (error?.status === 401) {
-        navigate("/401", { replace: true });
-      }
-      if (error?.status === 404) {
-        navigate("/404", { replace: true });
-      }
-      if (error?.status === 500) {
-        navigate("/500", { replace: true });
-      }
-      if (error?.status === null || error?.status === undefined) {
-        navigate("/500", { replace: true });
-      }
-    }
+    // if (isError) {
+    //   if (error?.status === 400) {
+    //     navigate("/400", { replace: true });
+    //   }
+    //   if (error?.status === 401) {
+    //     navigate("/401", { replace: true });
+    //   }
+    //   if (error?.status === 404) {
+    //     navigate("/404", { replace: true });
+    //   }
+    //   if (error?.status === 500) {
+    //     navigate("/500", { replace: true });
+    //   }
+    //   if (error?.status === null || error?.status === undefined) {
+    //     navigate("/500", { replace: true });
+    //   }
+    // }
+    console.log(error);
   }, [isError, error]);
 };
 
