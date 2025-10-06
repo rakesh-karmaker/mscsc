5ba2cc34430a975a0eb59e55c15b261ee2925103
rakesh-karmaker <rakeshkarmaker0175@gmail.com>
2025-10-06 23:08:17 +0600

    fix: correct import path for ArticleCard.css

--- client/src/components/home/articles/articleCard/ArticleCard.tsx
+++ client/src/components/home/articles/articleCard/ArticleCard.tsx
@@ -1,4 +1,3 @@
-import "./ArticleCard.css";
 import { NavLink } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
