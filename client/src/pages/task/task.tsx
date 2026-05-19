aec61a4e591450b78e9c9796bd4c731b9328ef6f
Rakesh Karmaker <rakeshkarmaker0175@gmail.com>
2026-05-19 22:07:33 +0600

    fix: error in layout of a task submission where the flex-box is not working

--- client/src/pages/task/task.tsx
+++ client/src/pages/task/task.tsx
@@ -76,7 +76,7 @@ export default function Task({
         setCanSubmit,
         setMode,
         setShowModeChange,
-        admin
+        admin,
       );
     }
   }, [isVerifying, isLoading, username]);
@@ -131,7 +131,7 @@ export default function Task({
     const answer = data?.content;
     const poster = data?.poster;
     const previousAnswer = task?.submissions?.find(
-      (s: Submission) => s.username === username
+      (s: Submission) => s.username === username,
     )?.answer;
     if (!answer && !previousAnswer) {
       toast.error("Nothing to submit");
@@ -177,7 +177,7 @@ export default function Task({
         {isLoading || isVerifying ? (
           <Loader />
         ) : (
-          <div>
+          <div className="flex gap-5 max-[1200px]:flex-col">
             <div className="task-section">
               <TaskHeader
                 task={task}
@@ -189,7 +189,7 @@ export default function Task({
               {mode === "preview" ? (
                 <TaskSubmissionPreview
                   submission={task?.submissions?.find(
-                    (s: Submission) => s.username === username
+                    (s: Submission) => s.username === username,
                   )}
                   task={task}
                 />
@@ -246,7 +246,7 @@ const changeStates = (
   setCanSubmit: Dispatch<SetStateAction<boolean>>,
   setMode: Dispatch<SetStateAction<"edit" | "preview">>,
   setShowModeChange: Dispatch<SetStateAction<boolean>>,
-  admin?: boolean
+  admin?: boolean,
 ) => {
   if (!task) {
     throw Error("Task not found");
